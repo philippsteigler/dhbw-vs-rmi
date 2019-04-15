7975d05803fc24cef03003aa11ba794bbe4ff91a
squanchy11 <philipp.steigler@mailbox.org>
2019-04-15 13:57:43 +0200

    Add error handling for client

--- src/main/Client.java
+++ src/main/Client.java
@@ -6,17 +6,21 @@ import java.rmi.Naming;
 
 public class Client {
 
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter IP: ");
-        String ip = br.readLine();
+    public static void main(String args[]) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter IP: ");
+            String ip = br.readLine();
 
-        ServerInterface obj = (ServerInterface) Naming.lookup("rmi://" + ip + ":1099" + "/My-Server");
+            ServerInterface obj = (ServerInterface) Naming.lookup("rmi://" + ip + ":1099" + "/My-Server");
 
-        // Test calculatePI
-        System.out.println(obj.calculatePI(10000000));
+            // Test calculatePI
+            System.out.println(obj.calculatePI(10000000));
 
-        // Test getServerOS
-        System.out.println(obj.getOS());
+            // Test getOS
+            System.out.println(obj.getOS());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
