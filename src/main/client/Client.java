489e56971a991ec02021d4531fb78405f26e9740
squanchy11 <philipp.steigler@mailbox.org>
2019-05-06 13:43:14 +0200

    Fixed etc/hosts always redirecting to localhost.

--- src/main/client/Client.java
+++ src/main/client/Client.java
@@ -1,22 +1,27 @@
 package main.client;
 
 import main.server.ServerInterface;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Client {
 
     public static void main(String args[]) {
         try {
+            // Read IP of remote server
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             System.out.print("Enter IP: ");
             String ip = br.readLine();
 
-            ServerInterface obj = (ServerInterface) Naming.lookup("rmi://" + ip + ":1099" + "/My-Server");
+            // Retrieve registry from remote server
+            Registry registry = LocateRegistry.getRegistry(ip, 1099);
 
-            // Test getOS
+            // get reference to remote object buy looking through the registry
+            ServerInterface obj = (ServerInterface) registry.lookup( "My-Server");
+
+            // Demo: call remote method from server
             System.out.println(obj.getOS());
         } catch (Exception e) {
             System.out.println("--- Error: " + e.getMessage());
--- src/main/server/Server.java
+++ src/main/server/Server.java
@@ -1,8 +1,10 @@
 package main.server;
 
-import java.rmi.Naming;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Server extends UnicastRemoteObject implements ServerInterface {
@@ -16,18 +18,28 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
         return System.getProperty("os.name");
     }
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
         System.out.println("--- RMI server started");
 
         try {
-            LocateRegistry.createRegistry(1099);
+            // Read IP of server network interface
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter IP: ");
+            String ip = br.readLine();
+
+            // Set hostname to server IP in order to overwrite localhost reference
+            System.setProperty("java.rmi.server.hostname", ip);
+
+            // Create Java RMI registry on port 1099
+            Registry registry = LocateRegistry.createRegistry(1099);
             System.out.println("--- Java RMI registry created.");
-        } catch (RemoteException e) {
+
+            // Initialize server object and bind it to registry
+            Server obj = new Server();
+            registry.rebind("My-Server", obj);
+            System.out.println("--- PeerServer bound in registry");
+        } catch (Exception e) {
             System.out.println("--- Java RMI registry already exists.");
         }
-
-        Server obj = new Server();
-        Naming.rebind("My-Server", obj);
-        System.out.println("--- PeerServer bound in registry");
     }
 }
